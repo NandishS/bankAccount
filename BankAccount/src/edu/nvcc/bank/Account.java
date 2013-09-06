248c1c3cda0441a0379d3dae998089ee891999d0
tkanchanawanchai6403 <tkanchanawanchai6403@AN093564-CT234.nvstu.edu>
2013-09-06 12:41:20 -0400

    initial

---
+++ BankAccount/src/edu/nvcc/bank/Account.java
@@ -0,0 +1,48 @@
+package edu.nvcc.bank;
+
+public class Account {
+	private double balance;
+	private String name;
+	private int acNum;
+	private static int count=0;
+	
+	public Account(double balance, String name, int acNum) {
+		this.balance = balance;
+		this.name = name;
+		this.acNum = acNum;
+		count++;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance += balance;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAcNum() {
+		return acNum;
+	}
+	public void setAcNum(int acNum) {
+		this.acNum = acNum;
+	}
+	public static int getCount() {
+		return count;
+	}
+	public static void setCount(int count) {
+		Account.count = count;
+	}
+
+	public String toString() {
+		return "Account [balance=" + balance + ", name=" + name + ", acNum="
+				+ acNum + "]";
+	}
+	
+	
+	
+}
---
+++ BankAccount/src/edu/nvcc/bank/AccountDemo.java
@@ -0,0 +1,26 @@
+package edu.nvcc.bank;
+
+import javax.swing.JOptionPane;
+
+public class AccountDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Account tanes = new Account(100.00,"Tanes",8888);
+		Account craig = new Account(1000000.00,"craig",0000);
+		
+		JOptionPane.showMessageDialog(null,tanes.getAcNum() + "\n" + tanes.getName() + "\n" + tanes.getBalance());
+		tanes.setBalance(30);
+		JOptionPane.showMessageDialog(null,tanes);
+		
+		JOptionPane.showMessageDialog(null,craig);
+		
+		
+		System.out.println("There are " + Account.getCount() + " account(s).");
+		Account.setCount(5);
+		System.out.println("There are " + tanes.getCount() + " account(s).");
+		System.out.println("There are " + craig.getCount() + " account(s).");
+		
+	}
+
+}
